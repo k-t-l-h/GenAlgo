b28eafe6fec79abf87c844a5c217834a8080900f
k-t-l-h <laciedreamer@gmail.com>
2021-07-11 12:47:25 +0300

    small fix

--- examples/main.go
+++ examples/main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	genalgo2 "genalgo"
+	genalg "github.com/k-t-l-h/GenAlgo"
 	"golang.org/x/exp/rand"
 	"math"
 )
 
 func main() {
 
-	sel := genalgo2.Panmixia{}
-	ga := genalgo2.GenAlgo{
+	sel := genalg.Panmixia{}
+	ga := genalg.GenAlgo{
 		MaxIteration: 4,
-		Generator:    &genalgo2.Generator{20},
-		Crossover: &genalgo2.UniformCrossover{
+		Generator:    &genalg.Generator{20},
+		Crossover: &genalg.UniformCrossover{
 			Probability:     0.5,
 			ProbabilityFunc: rand.Float64,
 		},
-		Mutate: &genalgo2.OneDotMutatation{
+		Mutate: &genalg.OneDotMutatation{
 			Probability:     0.5,
 			ProbabilityFunc: rand.Float64,
 		},
-		Schema: &genalgo2.Truncation{},
-		Fitness: func(unit genalgo2.BaseUnit) float64 {
+		Schema: &genalg.Truncation{},
+		Fitness: func(unit genalg.BaseUnit) float64 {
 			cr := unit.GetCromosomes()
 			fitness := 0.0
 			x := 0
@@ -40,6 +40,8 @@ func main() {
 		},
 		Select: &sel,
 	}
+	ga.OnBegin = func() {}
+	ga.OnEnd = func() {}
 	ga.Init(20)
 	ga.Simulation()
 
---
+++ go.mod
@@ -0,0 +1,8 @@
+module GenAlgo
+
+go 1.13
+
+require (
+	github.com/k-t-l-h/GenAlgo v0.0.0-20210531160918-6c46887774df
+	golang.org/x/exp v0.0.0-20210709195130-ecdcf02a369a
+)
